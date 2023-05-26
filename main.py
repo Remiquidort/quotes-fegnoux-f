3e4702898fb983ee043c70036ad6b1d2294388d4
Nico9417 <125251266+Nico9417@users.noreply.github.com>
2023-05-26 17:52:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,30 @@ from functions import *
 
 def menu():
     print("\n==== Programming Quotes ====")
-    print("1. Random quote")
-    print("2. All quotes")
-    print("3. Add a new quote")
-    print("4. Exit")
+    print("random : Random quote")
+    print("display : Display quotes")
+    print("add : Add a new quote")
+    print("exit : Exit the program")
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-4): ")
-        
-        if choice == "1":
+        choice = input(">> ")
+
+        if choice == "random":
             print_quote(random_quote(quotes))
-	    count = int(input("Enter the number of quotes to display: "))
-    	    display_quotes(quotes, count)
-        elif choice == "2":
-            view_quotes(quotes)
-	    count = int(input("Enter the number of quotes to display: "))
-	    display_quotes(quotes, count)        
-        elif choice == "4":
+        elif choice == "display":
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
+        elif choice == "add":
+            add_quote(quotes, "quotes.txt")
+        elif choice == "exit":
             print("Good bye...")
-	    count = int(input("Enter the number of quotes to display: "))
-	    display_quotes(quotes, count)
             break
         else:
             print("Invalid input")
-	    count = int(input("Enter the number of quotes to display: "))
-	    display_quotes(quotes, count)
-if __name__ == "__main__":
+
+if name == "main":
     main()
